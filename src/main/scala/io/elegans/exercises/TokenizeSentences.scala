98ad5502c14ed2ed492b0c0d7952ce65cc235591
Angelo Leto <angleto@gmail.com>
2016-11-22 19:12:00 +0100

    added SparseVector data type

--- src/main/scala/io/elegans/exercises/TokenizeSentences.scala
+++ src/main/scala/io/elegans/exercises/TokenizeSentences.scala
@@ -14,6 +14,7 @@ import scala.collection.JavaConversions._
 
 import org.apache.spark.storage.StorageLevel
 import org.apache.spark.rdd.RDD
+import org.apache.spark.mllib.linalg.{Vector, Vectors}
 
 object TokenizeSentences {
 
